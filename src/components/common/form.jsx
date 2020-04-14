1c631c4ebb9a19eceff63a5a55ff480e48aedc1a
Hariharan Ramanan <harryharanr@gmail.com>
2020-04-14 17:38:33 +0530

    Exercise 2 finished

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./input";
+import Select from "./select";
 class Form extends Component {
   state = {
     data: {},
@@ -63,8 +64,18 @@ class Form extends Component {
     );
   }
 
-  render() {
-    return null;
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
   }
 }
 
