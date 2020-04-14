85f3dbc313cb9c7590ef884f35aba64f3e2826c3
Hariharan Ramanan <harryharanr@gmail.com>
2020-04-14 11:09:05 +0530

    validate a form and vaildate a property implemented

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -11,34 +11,26 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
   validate = () => {
-    const options = { abortEarly: false };
-    const result = Joi.validate(this.state.account, this.schema, options);
-    console.log("Joi result", result);
+    const options = { abortEarly: false }; // If true , Joi terminates the rest of the validation when it finds the first validation error
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
     const errors = {};
-    const { account } = this.state;
-
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required.";
-      // ...
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required.";
-      // ...
-    }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
+
   handleChange = ({ currentTarget: input }) => {
     const errors = { ...this.state.errors };
     const errorMessage = this.validateProperty(input);
@@ -52,9 +44,8 @@ class LoginForm extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const errors = this.validate();
-    console.log("errors", errors);
     this.setState({ errors: errors || {} });
-    if (errors) return; // we don'r make a service call is error exists
+    if (errors) return; // we don't make a service call if error exists
   };
 
   render() {
