36249e32ebaa149b693dd16ad4b0aa1fc7eabdbe
Hariharan Ramanan <harryharanr@gmail.com>
2020-04-14 12:20:25 +0530

    Extracted helper rendering methods

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
-
+import Input from "./input";
 class Form extends Component {
   state = {
     data: {},
@@ -40,6 +40,28 @@ class Form extends Component {
     data[input.name] = input.value;
     this.setState({ data, errors });
   };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderInput(name, label, type = "text") {
+    const { data, errors } = this.state;
+    return (
+      <Input
+        name={name}
+        value={data[name]}
+        label={label}
+        type={type}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
 
   render() {
     return null;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
-import Input from "./common/input";
 class LoginForm extends Form {
   state = {
     data: {
@@ -20,31 +19,13 @@ class LoginForm extends Form {
     console.log("Form Submitted");
   };
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.username}
-            label="Username"
-            type="text"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            value={data.password}
-            label="Password"
-            type="password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-
-          <button disabled={this.validate()} className="btn btn-primary">
-            Login
-          </button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
