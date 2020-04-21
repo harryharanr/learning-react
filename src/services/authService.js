3edb4ac87f8244ce84828ea9b1bde07e6940f785
Hariharan Ramanan <harryharanr@gmail.com>
2020-04-21 20:29:19 +0530

    calling protected API endpoints by attaching jwt in request header

--- src/services/authService.js
+++ src/services/authService.js
@@ -26,9 +26,14 @@ export function getCurrentUser() {
   }
 }
 
+export function getJwt() {
+  return localStorage.getItem(tokenKey);
+}
+
 export default {
   login,
   loginWithJwt,
   logout,
   getCurrentUser,
+  getJwt,
 };
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import auth from "./authService";
+import { toast } from "react-toastify";
+
+axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
@@ -8,7 +12,7 @@ axios.interceptors.response.use(null, (error) => {
 
   if (!expectedError) {
     console.log("Logging the error", error);
-    alert("An unexpected error occured");
+    toast.error("An unexpected error occured");
   }
 
   return Promise.reject(error);
